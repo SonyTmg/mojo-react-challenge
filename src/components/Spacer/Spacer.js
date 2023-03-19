86bb4b7e9ec8ae5d37e6143ef5a0f76e7930641c
Sony Tamang <sony123ktm@gmail.com>
2023-03-19 21:59:05 +1100

    Add Spacer component

---
+++ src/components/Spacer/Spacer.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const variants = {
+  small: {
+    marginTop: 12
+  },
+  medium: {
+    marginTop: 24
+  },
+  large: {
+    marginTop: 42
+  }
+}
+
+function Spacer (props) {
+  return (
+    <div style={variants[props.size]}></div>
+  )
+}
+
+Spacer.propTypes = {
+  size: PropTypes.string
+}
+
+export default Spacer;
