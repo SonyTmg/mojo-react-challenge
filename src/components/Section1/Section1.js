9362e5207d4a32d43f0bd1410e41dd35ad0a1d6f
Sony Tamang <sony123ktm@gmail.com>
2023-03-19 15:12:12 +1100

    Add basic layout

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -1,10 +1,13 @@
 import Typography from '../Typography';
+import styles from './Section1.module.css';
 
 function Section1() {
   return (
-    <div>
-      <Typography>Section 1</Typography>
-    </div>
+    <section className={styles.wrapper}>
+      <div className={styles.container}>
+        <Typography>Section 1</Typography>
+      </div>
+    </section>
   );
 }
 
