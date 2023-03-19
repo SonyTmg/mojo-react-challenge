89f300873bfc24ff5d58300aa7d82e1d28e5d980
Sony Tamang <sony123ktm@gmail.com>
2023-03-19 16:16:08 +1100

    WIP: Initial hero section

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -1,11 +1,23 @@
 import Typography from '../Typography';
 import styles from './Section1.module.css';
+import iphone1 from './iphone-1.png';
+import iphone2 from './iphone-2.png';
 
 function Section1() {
   return (
     <section className={styles.wrapper}>
       <div className={styles.container}>
-        <Typography>Section 1</Typography>
+        <div className={styles.images}>
+          <div className={styles.iphoneLeft}>
+            <img src={iphone1} alt="Iphone" />
+          </div>
+          <div className={styles.iphoneRight}>
+            <img src={iphone2} alt="Iphone" />
+          </div>
+        </div>
+        <div className={styles.copy}>
+          Copy
+        </div>
       </div>
     </section>
   );
