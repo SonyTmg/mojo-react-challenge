f6ec38bdd254d37b3587bb681fc36cacf9e06476
Sony Tamang <sony123ktm@gmail.com>
2023-03-19 22:00:01 +1100

    Update spacing for section 1

--- src/components/Section1/Section1.js
+++ src/components/Section1/Section1.js
@@ -2,6 +2,8 @@ import Typography from '../Typography';
 import styles from './Section1.module.css';
 import iphone1 from './iphone-1.png';
 import iphone2 from './iphone-2.png';
+import Button from '../Button';
+import Spacer from '../Spacer';
 
 function Section1() {
   return (
@@ -19,9 +21,14 @@ function Section1() {
           <Typography variant="FuturaHeadingLarge">
             A simple approach to find happiness and confidence
           </Typography>
+          <Spacer size='small'/>
           <Typography variant="NewYorkHeadingXSmall">
             3 Questions and 3 Mindsets to change your life
           </Typography>
+          <Spacer size='medium'/>
+          <Button variant='large'>
+            Start Your Journey
+          </Button>
         </div>
       </div>
     </section>
