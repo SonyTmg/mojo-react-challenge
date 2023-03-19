e8df37113f02bc221fa9364c62bfebf52f206357
Sony Tamang <sony123ktm@gmail.com>
2023-03-19 15:33:37 +1100

    Add basic header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
-import Typography from '../Typography';
+// import Typography from '../Typography';
+import React from 'react';
+import styles from './Header.module.css';
+import logo from './logo.svg';
 
 function Header() {
   return (
-    <div>
-      <Typography>Header</Typography>
-    </div>
+    <header className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+        </div>
+        <div className={styles.right}>
+          <nav className={styles.nav}>
+            <a href="#" className={styles.navItem}>Home</a>
+            <a href="#" className={styles.navItem}>Course & App</a>
+            <a href="#" className={styles.navItem}>For Work</a>
+            <a href="#" className={styles.navItem}>Merch</a>
+            <a href="#" className={styles.navItem}>Contact us</a>
+            <a href="#" className={styles.navItem}>My account</a>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 }
 
