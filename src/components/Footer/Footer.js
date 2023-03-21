d6dbedec1c039d45ed1cb3a879be7f25d0c71bec
Sony Tamang <sony123ktm@gmail.com>
2023-03-21 13:27:47 +1100

    Initial work on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,22 @@
 import Typography from '../Typography';
+import React from 'react';
+import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <div>
-      <Typography>Footer</Typography>
-    </div>
+    <footer className={styles.wrapper}>
+        <div className={styles.container}>
+          <Typography variant='FuturaParagraphSmall'>
+            <nav className={styles.nav} role='navigation'>
+              <a href="#" className={styles.navItem}>Terms & Conditions</a>
+              <div className={styles.VerticalDivider}></div>
+              <a href="#" className={styles.navItem}>Privacy Policy</a>
+              <div className={styles.VerticalDivider}></div>
+              <a href="#" className={styles.navItem}> ©  Copyright of Mojo Crowe 2022</a>
+            </nav>
+          </Typography>
+        </div>
+      </footer>
   );
 }
 
